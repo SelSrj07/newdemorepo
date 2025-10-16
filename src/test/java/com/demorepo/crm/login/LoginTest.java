1308be8efcf4f935d0900098c6683272f59ac3c3
SelSrj07 <srhj5539@gmail.com>
2025-10-16 13:28:17 +0530

    new updated

--- src/test/java/com/demorepo/crm/login/LoginTest.java
+++ src/test/java/com/demorepo/crm/login/LoginTest.java
@@ -3,7 +3,8 @@ package com.demorepo.crm.login;
 public class LoginTest {
 	public static void main(String[] args) {
 		System.out.println("hello git bash");
-		System.out.println("hello git bash");
+		System.out.println("hello git bash2");
+		System.out.println("hello git bash3");
 	}
 
 }
